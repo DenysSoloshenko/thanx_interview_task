9cb266f5a6c57f650aa3b1da0ab092c646863d93
Denys Soloshenko <sdionis73@gmail.com>
2025-04-08 08:21:20 -0700

    Fixed package json, added root rote

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "rewards#index"
 
   namespace :api do
     get 'users/:id/show_points_balance', to: 'users#show_points_balance'
